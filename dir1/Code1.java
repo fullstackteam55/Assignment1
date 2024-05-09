913ec3b053aa5239ef5352bf4909928c5064b1ed
Nalini Kanta Pattanayak <nalinikanta.pattanayak@oracle.com>
2024-05-10 00:23:11 +0530

    Added directories dir1 and dir2

---
+++ dir1/Code1.java
@@ -0,0 +1,7 @@
+public class Code1 {
+
+public static void code1Method1 (){
+
+System.out.println("Code 1 Method1");
+}
+} 
---
+++ dir2/Code2.java
@@ -0,0 +1,10 @@
+public class Code2{
+
+
+public static void code2Method1(){
+
+System.out.println("Code 2 Method 1");
+}
+
+
+}
